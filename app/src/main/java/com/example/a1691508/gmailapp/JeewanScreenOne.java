260fae44b0d34885a5252305a359519782ecc22a
jeewanjot <jeewajotsingh09@gmail.com>
2017-05-20 15:05:01 -0400

    jeewan drawable commit

---
+++ app/src/main/java/com/example/a1691508/gmailapp/JeewanScreenOne.java
@@ -0,0 +1,13 @@
+package com.example.a1691508.gmailapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JeewanScreenOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeewan_screen_one);
+    }
+}

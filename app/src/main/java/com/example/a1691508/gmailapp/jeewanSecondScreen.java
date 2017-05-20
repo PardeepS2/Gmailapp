152d9a984e7dea3c45fdd5aa6affba7292fd2485
jeewanjot <jeewajotsingh09@gmail.com>
2017-05-20 17:32:10 -0400

    jeewan 6th commit

---
+++ app/src/main/java/com/example/a1691508/gmailapp/jeewanSecondScreen.java
@@ -0,0 +1,13 @@
+package com.example.a1691508.gmailapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jeewanSecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeewan_second_screen);
+    }
+}
